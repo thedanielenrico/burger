442f0f715e24354184b8fce8c18d8f3ffbb09d9a
thedanielenrico <thedanielenrico@gmail.com>
2019-04-17 18:15:16 -0700

    Start db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Boar Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
